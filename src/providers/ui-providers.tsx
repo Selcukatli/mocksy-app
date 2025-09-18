76e41025f77278db3d640f40ef317c7e66b9be82
Selcukatli <selcuk.atli@gmail.com>
2025-09-18 16:30:41 -0400

    Add profile debugging to diagnose production issue
    - Add verbose logging to ProfileProvider
    - Add ProfileDebug component with manual profile creation button
    - Temporarily show debug info in bottom-right corner
    
    This will help identify why profiles aren't being created in production.

--- src/providers/ui-providers.tsx
+++ src/providers/ui-providers.tsx
@@ -3,6 +3,7 @@
 import { useState, ReactNode } from 'react';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import Sidebar from '@/components/Sidebar';
+import { ProfileDebug } from '@/components/ProfileDebug';
 import { cn } from '@/lib/utils';
 
 interface UIProvidersProps {
@@ -24,6 +25,8 @@ export default function UIProviders({ children }: UIProvidersProps) {
         >
           {children}
         </div>
+        {/* Temporary debug component - remove after fixing */}
+        <ProfileDebug />
       </div>
     </ThemeProvider>
   );
